e2d046554463f73f7729e6cf5d4ad1b3c756664b
rodrigobatista-dev <rodrigobatista03@gmail.com>
2025-08-20 23:05:58 -0300

    Render update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://aula1conceitos.onrender.com/api",
+  baseURL: "https://aula1conceitos.onrender.com/",
 });
 
 export default api;
